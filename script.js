329a05683fb362b3a050d03cf78340e532b66a94
Bugra Sen <bugra.sen@bil.omu.edu.tr>
2023-08-13 01:20:30 +0300

    add

---
+++ script.js
@@ -0,0 +1,55 @@
+const url = "https://api.openweathermap.org/data/2.5/";
+const key = "a94df8162bfcbc26fce3bdd8fc2397fb";
+
+// input with enter key
+const setQuery = (e) => {
+  if (e.keyCode == "13") getResult(searchBar.value);
+};
+
+const searchBar = document.getElementById("searchBar");
+
+searchBar.addEventListener("keypress", setQuery);
+
+// Data extraction by city name
+const getResult = (cityName) => {
+  console.log(cityName);
+  let query = `
+  ${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr
+  `;
+  fetch(query)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResult);
+  searchBar.value = "";
+};
+
+//print data from api to screen
+const displayResult = (result) => {
+  console.log(result);
+  let city = document.querySelector(".city");
+  city.innerText = `${result.name} / ${result.sys.country}
+  `;
+
+  let temp = document.querySelector(".temp");
+  temp.innerText = `${Math.round(result.main.temp)}°C`;
+
+  let desc = document.querySelector(".desc");
+  desc.innerText = ` ${result.weather[0].description} `;
+
+  let wind = document.querySelector(".wind");
+  wind.innerText = ` ${result.wind.deg} / ${result.wind.speed}`;
+
+  let droplet = document.querySelector(".droplet");
+
+  droplet.innerText = ` ${result.main.humidity}`;
+
+  let icons = document.querySelector(".icons");
+  icons.classList.remove("hidden");
+
+  let weatherIcon = `images/${result.weather[0].icon}.png`;
+  console.log(weatherIcon);
+
+  let img = document.querySelector(".img");
+  img.src = weatherIcon;
+};
